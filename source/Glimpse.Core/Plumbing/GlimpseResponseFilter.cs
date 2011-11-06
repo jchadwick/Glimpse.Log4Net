daa066a0586307475e5c79c76699d1019ee726e8
David Pendray <davidpendray@gmail.com>
2011-11-06 22:32:17 +0000

    fixing bug where asp.net web forms calls filter twice and html before and including start of form is duplicated

--- source/Glimpse.Core/Plumbing/GlimpseResponseFilter.cs
+++ source/Glimpse.Core/Plumbing/GlimpseResponseFilter.cs
@@ -10,7 +10,6 @@ namespace Glimpse.Core.Plumbing
     //Heavily influenced by http://www.4guysfromrolla.com/articles/120308-1.aspx
     public class GlimpseResponseFilter : MemoryStream
     {
-        internal StringBuilder ResponseContent { get; set; }
         internal Stream OutputStream { get; set; }
         internal HttpContextBase Context { get; set; }
 
@@ -18,7 +17,6 @@ namespace Glimpse.Core.Plumbing
 
         public GlimpseResponseFilter(Stream output, HttpContextBase context)
         {
-            ResponseContent = new StringBuilder();
             OutputStream = output;
             Context = context;
         }
@@ -27,12 +25,9 @@ namespace Glimpse.Core.Plumbing
         {
             // Convert the content in buffer to a string
             string contentInBuffer = UTF8Encoding.UTF8.GetString(buffer);
-
             // Buffer content in responseContent until we reach the end of the page's markup
-            ResponseContent.Append(contentInBuffer);
 
-            var contentString = ResponseContent.ToString();
-            if (bodyEnd.IsMatch(contentString) && Context.GetGlimpseMode() == GlimpseMode.On)
+            if (bodyEnd.IsMatch(contentInBuffer) && Context.GetGlimpseMode() == GlimpseMode.On)
             {
                 var dataPath = HttpUtility.HtmlAttributeEncode(Context.GlimpseResourcePath("data.js") + "&id=" + Context.GetGlimpseRequestId());
                 var clientPath = HttpUtility.HtmlAttributeEncode(Context.GlimpseResourcePath("client.js"));
@@ -40,7 +35,7 @@ namespace Glimpse.Core.Plumbing
                 var html = string.Format(@"<script type='text/javascript' id='glimpseData' src='{0}'></script><script type='text/javascript' id='glimpseClient' src='{1}'></script></body>", dataPath, clientPath);
                 
                 // Add glimpse output script
-                string bodyCloseWithScript = bodyEnd.Replace(contentString,html);
+                string bodyCloseWithScript = bodyEnd.Replace(contentInBuffer,html);
 
                 // Write content to the outputStream
                 byte[] outputBuffer = UTF8Encoding.UTF8.GetBytes(bodyCloseWithScript);
